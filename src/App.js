0a3a09e367e31dcc71a0187f2c9e7e75e1ca3163
Thomas Verkempinck <thomas.fenalois@outlook.fr>
2021-10-14 20:57:36 +0200

    update query

--- src/App.js
+++ src/App.js
@@ -1,22 +1,71 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import Amplify, { API, graphqlOperation } from 'aws-amplify';
+import Lottie from 'react-lottie';
+import $ from 'jquery';
+
+import awsExports from './aws-exports';
+import { updateDoge } from './graphql/mutations';
+import { getDoge } from './graphql/queries';
+import animationData from './lottie/dogeMoon.json';
+
 import './App.css';
 
+Amplify.configure(awsExports);
+
 function App() {
+  const [dogePrice, setDogePrice] = useState(0);
+
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice"
+    }
+  };
+
+  async function fetchDogePrice() {
+    try {
+      const dogeData = await API.graphql(graphqlOperation(getDoge));
+      const currentPrice = dogeData.data.getDoge.price;
+      setDogePrice(currentPrice);
+    } catch (err) {
+      console.log("failed fetching data")
+      console.log(err);
+    }
+  }
+
+  async function updateDogePrice() {
+    try {
+      const dogeData = await API.graphql(graphqlOperation(getDoge));
+      const newDogePrice = dogeData.data.getDoge.price + 0.1;
+
+      const updatedDogePrice = await API.graphql(graphqlOperation(updateDoge, { input: newDogePrice }));
+
+      setDogePrice(updatedDogePrice.data.updateDoge.price);
+    } catch (err) {
+      console.log("failed to update the price! ");
+      console.log(err);
+    }
+  }
+
+  useEffect(() => { //initial fetch
+    fetchDogePrice()
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Dogecoin Price Predictor</h1>
+        <p> One click = 10 cents</p>
+        <h2>$ {dogePrice.toFixed(2)}</h2>
+        <button onClick={updateDogePrice}>Doge to the 🌜!</button>
+
+        <Lottie
+          options={defaultOptions}
+          height="30%"
+          width="30%"
+        />
       </header>
     </div>
   );
